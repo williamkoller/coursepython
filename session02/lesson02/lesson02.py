4ef61587189791718b312cd3d209a6c94b06d34b
william koller <wkoller25@gmail.com>
2021-09-24 11:24:50 -0300

    feat: add lesson 02

---
+++ session02/lesson02/lesson02.py
@@ -0,0 +1,15 @@
+print(123456)
+# sep is a separator
+print('William', 'Koller', sep='-', end='-')
+print('Joao', 'and', 'Maria', sep='-', end='')
+
+
+print('\n-------###--------')
+
+print(346, 119, 170, sep='.', end='-')
+print(10)
+
+name = 'Pietro'
+surname = 'Koller'
+
+print(name, surname)
