7bbf3ca96b8d4c4a5a7be16fd4bbb53ba3e3274e
william koller <wkoller25@gmail.com>
2021-10-04 21:36:58 -0300

    feat: add lesson 04

---
+++ session02/lesson04/lessson04.py
@@ -0,0 +1,24 @@
+"""
+Tipos de dados
+str - string - textos 'Assim' "Assim"
+int - int - 12456 0 -10 -20 50 6000 -15000
+float - real/ponto - 10.50 -10.50 1.5
+bool - booleano/lógico - True/False
+"""
+
+print('William', type('William'))
+print(32, type(32))
+print(1.85, type(1.85))
+print(10 == 10, type(10 == 10))
+print('l' == 'L', type('l' == 'L'))
+print(bool(''))
+print('william', type('william'), bool('william'))
+
+"""
+
+type casting 
+
+"""
+
+print('10', type('10'), type(int('10')))
+
