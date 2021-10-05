384b2f0baa1331e2730c238a947d46b537e9f9c6
william koller <wkoller25@gmail.com>
2021-10-05 07:51:36 -0300

    feat: add lesson 05

---
+++ session02/lesson05/lesson05.py
@@ -0,0 +1,17 @@
+""""
+operadores aritméticos
++, -, *, /,  //, **, %, ()
+"""
+value_default = 10
+
+print('multiplicação *', value_default * 10)
+print('subtração -', value_default - 5)
+print('adição +', value_default + 10)
+print('divisão /', value_default / 12)
+print('divisão // retornando numero inteiro', 10.5 // 3)
+print('elevado **', value_default ** 2)
+print('resto da divisão', value_default % 3)
+print('()', (value_default * 2) + 5)
+
+
+print('type casting multiplicação *', value_default * int('10'))
