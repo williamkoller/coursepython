618a6c0df4c74cbf040f65ad66e0ea922958829a
william koller <wkoller25@gmail.com>
2021-10-13 09:11:08 -0300

    feat: add lesson 09

---
+++ session02/lesson09/lesson09.py
@@ -0,0 +1,24 @@
+"""
+ * Entrada de Dados
+"""
+
+# nome = input('Qual seu nome? ')
+
+# print(f'O usuário digitou {nome} e o tipo da variável é', f'{type(nome)}')
+
+
+# nome = input('Qual seu nome? ')
+# idade = input('Qual sua idade? ')
+
+# ano_nascimento = 2021-int(idade)
+
+# print('')
+
+# print(f'{nome} tem {idade} anos e nasceu em {ano_nascimento}.')
+
+
+numero_1 = int(input('Digite um número: '))
+numero_2 = input('Digite outro numero: ')
+numero_2 = int(numero_2)
+
+print(numero_1 + numero_2)
